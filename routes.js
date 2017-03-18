adc1ae1822a0df717160d2427a97120bbf8dfe20
Oleg Korol <oleg@olegkorol.de>
2017-03-18 01:39:51 +0100

    Add BotKit, work in progress...

--- routes.js
+++ routes.js
@@ -1,34 +1,12 @@
 'use strict';
 
-const Conversation = require('./conversation');
+//const Conversation = require('./conversation');
 
 module.exports = function (router) {
 
     router.get('/test', function (req, res) {
 
-        // This example makes two successive calls to conversation service.
-        // Note how the context is passed:
-        // In the first message the context is undefined. The service starts a new conversation.
-        // The context returned from the first call is passed in the second request - to continue the conversation.
-        Conversation.sendMessage('Hi!', undefined)
-            .then(response1 => {
-                // APPLICATION-SPECIFIC CODE TO PROCESS THE DATA
-                // FROM CONVERSATION SERVICE
-                console.log(JSON.stringify(response1, null, 2), '\n--------');
-
-                // invoke a second call to conversation
-                return Conversation.sendMessage('Tell me a joke', response1.context);
-            })
-            .then(response2 => {
-                console.log(JSON.stringify(response2, null, 2), '\n--------');
-                //console.log('Note that the two reponses should have the same context.conversation_id');
-                res.status(200).json(response2);
-            })
-            .catch(err => {
-                // APPLICATION-SPECIFIC CODE TO PROCESS THE ERROR
-                // FROM CONVERSATION SERVICE
-                console.error(JSON.stringify(err, null, 2));
-            });
+        res.sendStatus(200);
     })
 
-}
+};
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const express = require('express');
-const app     = express();
-const port    = 4205;
-const router  = express.Router();
-const routes  = require('./routes');
+const app = express();
+const port = process.env.PORT || 4205;
+const router = express.Router();
+const routes = require('./routes');
+const chatbot = require('./botkit');
 
 // pass routes.js to router and initialize
 routes(router);
 app.use('/', router);
 
 // start server
-app.listen(port, function(req, res) {
+app.listen(port, function (req, res) {
     console.info(`Started Express server on port ${port}`)
-})
+});
